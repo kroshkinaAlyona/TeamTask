c9b9fce40be098bbe41e2a89222aa4fa7924102a
Kroshkina.alena <Kroshkina.alena@gmail.com>
2020-06-27 03:41:24 +0300

    ffix

--- ElectronicJournal/TestTask/Program.cs
+++ ElectronicJournal/TestTask/Program.cs
@@ -17,6 +17,7 @@ namespace TestTask
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            ываываыва
         }
     }
 }
