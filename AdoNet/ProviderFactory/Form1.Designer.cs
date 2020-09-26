49ece4f882a327117f329bc55cfea8cf911e3325
Kroshkina.alena <Kroshkina.alena@gmail.com>
2020-09-26 17:15:07 +0300

    fix

---
+++ AdoNet/ProviderFactory/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ProviderFactory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(634, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 219);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(391, 119);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Async Callback";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(508, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Transaction";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(280, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SQL request:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(9, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Connection string:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(433, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Get All Providers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(480, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Execute request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(9, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(212, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(9, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(465, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "select * from Books";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox1.Location = new System.Drawing.Point(9, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(604, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(628, 219);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ AdoNet/ProviderFactory/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProviderFactory
+{
+    public partial class Form1 : Form
+    {
+        DbConnection conn = null;
+        DbProviderFactory fact = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable t = DbProviderFactories.GetFactoryClasses();
+            dataGridView1.DataSource = t;
+            comboBox1.Items.Clear();
+            foreach (DataRow dr in t.Rows)
+            {
+                comboBox1.Items.Add(dr["InvariantName"]);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = GetConnectionStringByProvider(comboBox1.SelectedItem.ToString());
+
+                fact = DbProviderFactories.GetFactory(comboBox1.SelectedItem.ToString());
+                conn = fact.CreateConnection();
+                conn.ConnectionString = textBox1.Text;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+        private string GetConnectionStringByProvider(string providerName)
+        {
+            var settings = ConfigurationManager.ConnectionStrings;
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    if (cs.ProviderName == providerName)
+                        return cs.ConnectionString;
+                }
+            }
+            return string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var adapter = fact.CreateDataAdapter();
+                adapter.SelectCommand = conn.CreateCommand();
+                adapter.SelectCommand.CommandText = textBox2.Text.ToString();
+
+                var set = new DataSet();
+                adapter.Fill(set, "Books");
+
+                DataViewManager dvm = new DataViewManager(set);
+                //dvm.DataViewSettings["Books"].RowFilter   = $"Id < 10";
+                dvm.DataViewSettings["Books"].Sort = "Title ASC";
+                var view = dvm.CreateDataView(set.Tables["Books"]);
+
+                dataGridView1.DataSource = view;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Any())
+                button1.Enabled = true;
+            else
+                button1.Enabled = false;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {          
+            DbTransaction tran = null;
+            try
+            {
+                conn.Open();
+                
+                using (var comm = conn.CreateCommand())
+                {
+                    tran = conn.BeginTransaction();
+
+                    comm.Transaction = tran;
+                    comm.CommandText = @"create table tmp3(id int not null identity(1,1) primary key, f1 varchar(100), f2 int)";
+                    comm.ExecuteNonQuery();
+                    comm.CommandText = @"insert into tmp3(f1, f2) values('Text value 1', 555)";
+                    comm.ExecuteNonQuery();
+                    comm.CommandText = @"insert into tmp3(f1, f2) values('Text value for second row', 777)";
+                    comm.ExecuteNonQuery();
+
+                    tran.Commit();
+                }                                
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+                tran.Rollback();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            const string asyncEnable = "Asynchronous Processing=true";
+            if (!textBox1.Text.Contains(asyncEnable))
+            {
+                textBox1.Text = string.Format("{0};{1}", textBox1.Text, asyncEnable);
+            }
+            conn.ConnectionString = textBox1.Text;
+            conn.Open();
+
+            using (var comm = (conn as SqlConnection).CreateCommand())
+            {
+                comm.CommandText = $"WAITFOR DELAY '00:00:05';{textBox2.Text};";
+                comm.CommandType = CommandType.Text;
+                //comm.CommandTimeout = 10;
+              
+                comm.BeginExecuteReader(Callback, comm);
+                MessageBox.Show("Added thread is working...");
+            }
+        }
+
+        DataTable table = null;
+        private void Callback(IAsyncResult result)
+        {
+            try
+            {
+                SqlCommand command = (SqlCommand)result.AsyncState;
+                var dataReader = command.EndExecuteReader(result);
+
+                table = new DataTable();
+                do
+                {
+                    for (int i = 0; i < dataReader.FieldCount; i++)
+                        table.Columns.Add(dataReader.GetName(i));
+
+                    while (dataReader.Read())
+                    {
+                        var row = table.NewRow();
+                        for (int i = 0; i < dataReader.FieldCount; i++)
+                            row[i] = dataReader[i];
+
+                        table.Rows.Add(row);
+                    };
+                }
+                while (dataReader.NextResult());
+
+                if (conn != null)
+                    conn.Close();
+               
+                ShowData();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Callback" + exp.ToString());
+            }
+        }
+
+        private void ShowData()
+        {
+            if (dataGridView1.InvokeRequired)
+            {
+                dataGridView1.Invoke(new Action(ShowData));
+                return;
+            }
+            dataGridView1.DataSource = table;
+        }
+    }
+}
