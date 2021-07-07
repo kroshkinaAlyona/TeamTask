65f406332ea83f2e94cee880bfbb2cb06d826e5b
Kroshkina.alena <Kroshkina.alena@gmail.com>
2021-07-07 19:49:21 +0300

    fix digit

--- OOP/OOP/OOP.cpp
+++ OOP/OOP/OOP.cpp
@@ -1,7 +1,37 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+int Div(int a, int b)
+{
+    try
+    {
+        if (b == 0)
+        {
+            throw exception("Divided by zero!");
+        }
+        return a / b;
+    }
+    catch (...)
+    {
+        cout << "Divided by zero!" << endl;
+        throw;
+    }
+}
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    try
+    {
+        int c = Div(1, 0);
+        cout << c << endl;
+    }
+    catch (exception exp)
+    {
+        cout << "Exeption in fun Div :" << exp.what() << endl;
+    }
 }
 
